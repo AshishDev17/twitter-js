0abe7bcd7a0bdee0d0f368896a84dca142107713
Ashish Malav <aashmalav@gmail.com>
2017-08-01 20:01:51 -0500

    completed till setting up nunjuck engine

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 const app = express();
 
 
@@ -7,10 +8,15 @@ const app = express();
 //   console.log('requested', req.method, req.path);
 // });
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
 app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
-  res.send('Home Page!');
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
 
 app.get('/news', (req, res, next) => {
